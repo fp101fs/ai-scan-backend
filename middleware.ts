233115a05f0f6371d77482cff9742aa9e256c9ac
fp101fs <136630859+fp101fs@users.noreply.github.com>
2026-08-01 17:45:45 +0700

    fix: set middleware to nodejs runtime, use NextResponse

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,17 @@
-import { authOptions } from "./auth";
 import { getServerSession } from "next-auth/next";
+import { authOptions } from "../../auth";
+import { NextResponse } from "next/server";
+
+export const config = {
+  matcher: ["/dashboard/:path*"],
+};
 
 export async function middleware() {
   const session = await getServerSession(authOptions);
 
   if (!session) {
-    return Response.redirect(
-      new URL("/", process.env.VERCEL_URL || "http://localhost:3000")
-    );
+    return NextResponse.redirect(new URL("/", process.env.VERCEL_URL || "http://localhost:3000"));
   }
 }
 
-export const config = {
-  matcher: ["/dashboard/:path*"],
-};
+export const runtime = "nodejs";
